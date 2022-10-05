6c06aa99ed7d490de69b420312c5fc871c9960a1
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-10-05 15:10:01 -0400

    First commit

---
+++ InternshipProject/InternshipProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternshipProjectApp.swift
+//  InternshipProject
+//
+//  Created by Christopher Koski on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct InternshipProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
